08f68c6092fb18a2529c6beb85165234065f6358
haider981 <haider@vkgp.in>
2025-09-18 10:12:31 +0530

    Update spocAddProjectController.js

--- backend/src/controllers/spocAddProjectController.js
+++ backend/src/controllers/spocAddProjectController.js
@@ -1,5 +1,6 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+// const { PrismaClient } = require('@prisma/client');
+// const prisma = new PrismaClient();
+const prisma = require("../config/prisma");
 
 // Create a new project
 const createProject = async (req, res) => {
