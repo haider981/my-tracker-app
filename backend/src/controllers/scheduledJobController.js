a8f5443cf10038cb11cd07c7df8dc1a3efeb6a1e
haider981 <haider@vkgp.in>
2025-10-15 10:26:31 +0530

    Update scheduledJobController.js

--- backend/src/controllers/scheduledJobController.js
+++ backend/src/controllers/scheduledJobController.js
@@ -21,7 +21,7 @@ exports.autoSubmitWorklogsAndAssignLeave = async () => {
     const allUsers = await prisma.users.findMany({
       where: {
         team: {
-            in: ["Editorial_Maths", "Editorial_Science", "Editorial_SST","Editorial_English","CSMA_Maths"]
+            in: ["Editorial_Maths", "Editorial_Science", "Editorial_SST","Editorial_English"]
           }
       },
       select: {
