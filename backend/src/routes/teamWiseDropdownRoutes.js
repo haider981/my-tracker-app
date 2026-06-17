daf05143299b63a1d27a7854660b142646d0fcf4
haider981 <haiderzaidi45h@outlook.com>
2026-06-17 11:40:10 +0530

    Update teamWiseDropdownRoutes.js

--- backend/src/routes/teamWiseDropdownRoutes.js
+++ backend/src/routes/teamWiseDropdownRoutes.js
@@ -5,9 +5,25 @@ const dropdownController = require('../controllers/teamWiseDropdownController');
 
 // Team routes
 router.get('/teams/all', authenticateToken, dropdownController.getAllTeams);
+router.get('/search/global', authenticateToken, dropdownController.globalSearchDropdownValues);
 router.get('/:teamName', authenticateToken, dropdownController.getDropdownDataByTeam);
 router.post('/', authenticateToken, dropdownController.addDropdownValue);
 router.put('/', authenticateToken, dropdownController.updateDropdownValue);
 router.delete('/', authenticateToken, dropdownController.deleteDropdownValue);
 
 module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+// const authenticateToken = require('../middleware/auth');
+// const dropdownController = require('../controllers/teamWiseDropdownController');
+
+// // Team routes
+// router.get('/teams/all', authenticateToken, dropdownController.getAllTeams);
+// router.get('/:teamName', authenticateToken, dropdownController.getDropdownDataByTeam);
+// router.post('/', authenticateToken, dropdownController.addDropdownValue);
+// router.put('/', authenticateToken, dropdownController.updateDropdownValue);
+// router.delete('/', authenticateToken, dropdownController.deleteDropdownValue);
+
+// module.exports = router;
